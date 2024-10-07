81c46bf9d396e18e315aee57c659f1b2dc8675ac
Marcos Alan <marcos38alan@gmail.com>
2024-10-07 10:55:43 -0300

    atualizando projeto

---
+++ exerciciosResolvidos/ExerciciosBasicos/ex02.py
@@ -0,0 +1,40 @@
+'''
+Exercício para iniciantes:
+Neste arquivo passarei várias opções para mostrar na tela
+'''
+
+print("Olá meu nome é Marcos DV do canal do youtube @acessibilidadetotal")
+
+'''
+mas podemos mostrar o resultado a cima de outra maneira
+com uso de variáveis onde
+elas irão armazenar os dados para integrar ao print de uma 
+forma mais eficiente e com um resultado igual.
+'''
+
+#exemplo
+
+nome = "Marcos DV"
+canal_youtube = '@acessibilidadetotal'
+
+print(f"Olá, meu nome é {nome} e meu canal no youtube é {canal_youtube}")
+
+'''
+Agora a demonstração usando as mesmas variáveis
+só mostrando os resultados separados,
+um em cada linha.
+'''
+
+#exemplo
+
+print(f"Olá: meu nome é {nome}\n meu canal do youtube é {canal_youtube}")
+
+
+'''
+as 3 respostas apresentadas são:
+
+Olá meu nome é Marcos DV do canal do youtube @acessibilidadetotal
+Olá, meu nome é Marcos DV e meu canal no youtube é @acessibilidadetotal
+Olá: meu nome é Marcos DV
+ meu canal do youtube é @acessibilidadetotal
+'''
